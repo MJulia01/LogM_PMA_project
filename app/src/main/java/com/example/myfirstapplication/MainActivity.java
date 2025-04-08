759ba0e436b7c7e93e164069e9f712dd4c6c2bd9
MJulia01 <malgosia.u@interia.eu>
2025-04-08 16:10:26 +0200

    Added 2 new activities, browse and shakeIt

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myfirstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,13 +25,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Button button = (Button) findViewById(R.id.addRecipyButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the addRecipyButton");
+                Intent intent = new Intent(MainActivity.this, Add_Recipy.class);
+                startActivity(intent);
+            }
+        });
 
+        Button button2 = (Button) findViewById(R.id.browseButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the browseButton");
+                Intent intent = new Intent(MainActivity.this, Browse_Recipes.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.shakeITButton);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the shakeITButton");
+                Intent intent = new Intent(MainActivity.this, Shake_Recipes.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
-public void browseN(View view) {
-    Log.d("BUTTONS", "User tapped the BUTOOOONNNNN");
-}
+
 
 
 
